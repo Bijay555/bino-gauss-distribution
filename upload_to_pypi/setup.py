67cbd641945a7d3920ae4421faf40b84b3f90b6f
Bijay Adhikari <44121999+Bijay555@users.noreply.github.com>
2020-06-08 00:23:15 +0545

    Add files via upload

---
+++ upload_to_pypi/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='bino_gauss_distribution',
+      version='0.2',
+      description='Binomial & Gaussian distributions',
+      packages=['bino_gauss_distribution'],
+      author='Bijay Adhikari',
+      author_email='bjsharma555@gmail.com',
+      url="https://github.com/Bijay555/bino-gauss-distribution",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+      zip_safe=False)
